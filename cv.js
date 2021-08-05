ba64081a9adec16a6d95a7bf9dbd065d5e774aa9
omar <fikriomar60@gmail.com>
2021-08-05 19:25:23 +0700

    Fixed some bug

--- cv.js
+++ cv.js
@@ -72,19 +72,19 @@ window.addEventListener('DOMContentLoaded', event => {
 $(document).ready(function() {
 	// Prevent (#) in URL
 	$("a").on('click', function(event) {
-		if (this.hash !== "") {
+		// Store hash
+		var hash = this.hash;
+		if (hash !== "") {
 			// Prevent default anchor click behavior
 			event.preventDefault();
-			// Store hash
-			var hash = this.hash;
 			// Using jQuery's animate() method to add smooth page scroll
 			// The optional number (10) specifies the number of milliseconds it takes to scroll to the specified area
 			$('html, body').animate({
 				scrollTop: $(hash).offset().top
-			}, 20, function(){
-			// Add hash (#) to URL when done scrolling (default click behavior)
-			window.location.hash = "";
-		});
+			}, 1500, function(){
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				// window.location.hash = hash;
+			});
 		}
 	});
 	setTimeout(function() {portoku();}, 2500);
